8fae83cd2580ec2d3cbf0a665c149a9ca677eb1d
willbio <ricardowillbio77@gmail.com>
2026-02-05 14:50:52 -0300

    # Changes to be committed: #	new file:   verce.json #	modified:   vite.config.ts #

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Garante que os caminhos funcionem na raiz
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Cria o atalho @ para a pasta src
