61c6c81f2a4908789143817213ab6ac965f40324
willbio <ricardowillbio77@gmail.com>
2026-02-06 14:21:23 -0300

    # Changes to be committed: #	modified:   index.css #	modified:   src/index.tsx #

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import './index.css'; // <--- ESTA É A LINHA QUE FALTAVA!
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
